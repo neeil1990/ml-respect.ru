37b70cfd901a4d2a59744786ffa5e47abc85303c
neeil1990 <neeil@mail.ru>
2021-05-20 14:58:48 +0300

    #116742 new detail template

--- local/templates/mlrespect/components/prime/news/news/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/mlrespect/components/prime/news/news/bitrix/news.detail/.default/result_modifier.php
@@ -20,9 +20,13 @@ foreach($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'] as $idx=>$img)
     if(!empty($img['SRC']))
     {
 		$renderImage = CFile::ResizeImageGet($img, array("width" => IMG_CART_DETAIL_BIG_WIDTH, "height" => IMG_CART_DETAIL_BIG_HEIGHT), BX_RESIZE_IMAGE_EXACT, false);
-        $arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'][$idx]['SRC'] = $renderImage['src']; 
+        $arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'][$idx]['SRC'] = $renderImage['src'];
 
 		$renderImagePrew = CFile::ResizeImageGet($img, array("width" => IMG_CART_DETAIL_PREW_WIDTH, "height" => IMG_CART_DETAIL_PREW_HEIGHT), BX_RESIZE_IMAGE_EXACT, false);
-        $arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'][$idx]['SRC_PREW'] = $renderImagePrew['src'];       
+        $arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'][$idx]['SRC_PREW'] = $renderImagePrew['src'];
     }
 }
+
+if($arResult['DISPLAY_PROPERTIES']['equipment']['VALUE'])
+    $arResult['equipment'] = array_chunk($arResult['DISPLAY_PROPERTIES']['equipment']['VALUE'], ceil(count($arResult['DISPLAY_PROPERTIES']['equipment']['VALUE'])/4));
+
