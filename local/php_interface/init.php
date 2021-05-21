f46c1aa1d17280eb344c2519fd86c3329a1a836c
neeil1990 <neeil@mail.ru>
2021-05-21 13:00:31 +0300

    #116742 clear cache

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -12,5 +12,30 @@ define(IMG_CART_DETAIL_PREW_HEIGHT, 79);
 define(IMG_MENU_SPECIAL_WIDTH, 118);
 define(IMG_MENU_SPECIAL_HEIGHT, 79);
 
+function myscandir($dir)
+{
+    $list = scandir($dir);
+    unset($list[0],$list[1]);
+    return array_values($list);
+}
+
+function clear_dir($dir)
+{
+    $list = myscandir($dir);
+
+    foreach ($list as $file)
+    {
+        if (is_dir($dir.$file))
+        {
+            clear_dir($dir.$file.'/');
+            rmdir($dir.$file);
+        }
+        else
+        {
+            unlink($dir.$file);
+        }
+    }
+}
+
 require_once __DIR__.'/events.php';
 ?>
