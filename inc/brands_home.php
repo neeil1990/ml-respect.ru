8cef4ec1ffc6f5b00fc3acb303f28e602bbaac5a
/usr/bin/vim <support@beget.ru>
2021-05-26 15:09:31 +0300

    srv

--- inc/brands_home.php
+++ inc/brands_home.php
@@ -1,17 +1,12 @@
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
+            <a href="/car/?arrFilter_pf%5Bbrand%5D=audi&set_filter=Y">Audi</a>
+            </div><div class="car_list_item_col">2</div>
+            </div>
+<div class="car_list_item clearfix">
+            <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=bmw&set_filter=Y">Bmw</a>
             </div><div class="car_list_item_col">2</div>
-            </div>
-<div class="car_list_item clearfix">
-            <div class="car_list_item_name">
-            <a href="/car/?arrFilter_pf%5Bbrand%5D=chevrolet&set_filter=Y">Chevrolet</a>
-            </div><div class="car_list_item_col">2</div>
-            </div>
-<div class="car_list_item clearfix">
-            <div class="car_list_item_name">
-            <a href="/car/?arrFilter_pf%5Bbrand%5D=citroen&set_filter=Y">Citroen</a>
-            </div><div class="car_list_item_col">1</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
@@ -21,12 +16,7 @@
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=hyundai&set_filter=Y">Hyundai</a>
-            </div><div class="car_list_item_col">5</div>
-            </div>
-<div class="car_list_item clearfix">
-            <div class="car_list_item_name">
-            <a href="/car/?arrFilter_pf%5Bbrand%5D=infiniti&set_filter=Y">Infiniti</a>
-            </div><div class="car_list_item_col">1</div>
+            </div><div class="car_list_item_col">4</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
@@ -36,17 +26,12 @@
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=jeep&set_filter=Y">Jeep</a>
-            </div><div class="car_list_item_col">1</div>
+            </div><div class="car_list_item_col">2</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=kia&set_filter=Y">Kia</a>
-            </div><div class="car_list_item_col">19</div>
-            </div>
-<div class="car_list_item clearfix">
-            <div class="car_list_item_name">
-            <a href="/car/?arrFilter_pf%5Bbrand%5D=lada+%28%D0%B2%D0%B0%D0%B7%29&set_filter=Y">Lada (ваз)</a>
-            </div><div class="car_list_item_col">1</div>
+            </div><div class="car_list_item_col">18</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
@@ -61,7 +46,7 @@
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=mercedes-benz&set_filter=Y">Mercedes-benz</a>
-            </div><div class="car_list_item_col">1</div>
+            </div><div class="car_list_item_col">3</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
@@ -71,16 +56,16 @@
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=nissan&set_filter=Y">Nissan</a>
-            </div><div class="car_list_item_col">5</div>
-            </div>
-<div class="car_list_item clearfix">
-            <div class="car_list_item_name">
-            <a href="/car/?arrFilter_pf%5Bbrand%5D=renault&set_filter=Y">Renault</a>
             </div><div class="car_list_item_col">2</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
-            <a href="/car/?arrFilter_pf%5Bbrand%5D=skoda&set_filter=Y">Skoda</a>
+            <a href="/car/?arrFilter_pf%5Bbrand%5D=opel&set_filter=Y">Opel</a>
+            </div><div class="car_list_item_col">1</div>
+            </div>
+<div class="car_list_item clearfix">
+            <div class="car_list_item_name">
+            <a href="/car/?arrFilter_pf%5Bbrand%5D=renault&set_filter=Y">Renault</a>
             </div><div class="car_list_item_col">1</div>
             </div>
 <div class="car_list_item clearfix">
@@ -101,15 +86,15 @@
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=toyota&set_filter=Y">Toyota</a>
-            </div><div class="car_list_item_col">2</div>
+            </div><div class="car_list_item_col">3</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=volkswagen&set_filter=Y">Volkswagen</a>
-            </div><div class="car_list_item_col">8</div>
+            </div><div class="car_list_item_col">7</div>
             </div>
 <div class="car_list_item clearfix">
             <div class="car_list_item_name">
             <a href="/car/?arrFilter_pf%5Bbrand%5D=volvo&set_filter=Y">Volvo</a>
-            </div><div class="car_list_item_col">5</div>
+            </div><div class="car_list_item_col">9</div>
             </div>
