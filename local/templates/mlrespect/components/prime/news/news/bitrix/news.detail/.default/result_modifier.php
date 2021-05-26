94d0b3ff0c1e0e4de7d1bdd4f6b0f6a1ac04fdc9
neeil1990 <neeil@mail.ru>
2021-05-26 12:10:04 +0300

    #116742 speed page detail

--- local/templates/mlrespect/components/prime/news/news/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/mlrespect/components/prime/news/news/bitrix/news.detail/.default/result_modifier.php
@@ -1,21 +1,13 @@
 <?php
-
 $arSelect = Array("NAME", "PROPERTY_address", "PROPERTY_phone", "PROPERTY_coord", "PROPERTY_CITY");
-$arFilter = Array("IBLOCK_ID"=>4, "ACTIVE"=>"Y","ID"=>$arResult['DISPLAY_PROPERTIES']['filial']['VALUE']);
+$arFilter = Array("IBLOCK_ID" => $arResult['DISPLAY_PROPERTIES']['filial']['LINK_IBLOCK_ID'], "ACTIVE" => "Y","ID" => $arResult['DISPLAY_PROPERTIES']['filial']['VALUE']);
+$res = CIBlockElement::GetList(array("SORT" => "ASC"), $arFilter, false, Array("nTopCount" => 1), $arSelect);
+if($arFields = $res->Fetch())
+    $arResult["FILIAL"] = $arFields;
 
-$res = CIBlockElement::GetList(array("SORT" => "ASC"), $arFilter, false, Array("nPageSize"=>PHP_INT_MAX), $arSelect);
-
-$filial_items = array();
-while($ob = $res->GetNextElement())
-{
-  $arFields = $ob->GetFields();
-  $filial_items[] = $arFields;
-}
-
-$arResult["FILIAL"] = $filial_items;
 
 // Олег: изменяем размер картинок
-foreach($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'] as $idx=>$img)
+foreach($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'] as $idx => $img)
 {
     if(!empty($img['SRC']))
     {
--- local/templates/mlrespect/components/prime/news/news/bitrix/news.detail/.default/template.php
+++ local/templates/mlrespect/components/prime/news/news/bitrix/news.detail/.default/template.php
@@ -12,31 +12,31 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-
-<?$cnaonical = explode('?', $arResult["DETAIL_PAGE_URL"]);
-$APPLICATION->AddHeadString('<link href="https://'.$_SERVER["SERVER_NAME"].$cnaonical[0].'" rel="canonical" />',true); // Олег: установка канонического url?>
-<div itemscope itemtype="http://schema.org/Product" class="card_car" style="margin-top: 30px;"
-    <?if(!empty($arResult['DISPLAY_PROPERTIES']['panorama']['VALUE'])):?>
-        <?if(!empty($arResult['DISPLAY_PROPERTIES']['vin']['VALUE'])):?>data-vin="<?=$arResult['DISPLAY_PROPERTIES']['vin']['VALUE']?>"<?endif;?>
-    <?endif;?>
+<div itemscope itemtype="http://schema.org/Product"
+     class="card_car"
+     style="margin-top: 30px;"
+    <?if(!empty($arResult['DISPLAY_PROPERTIES']['vin']['VALUE'])):?> data-vin="<?=$arResult['DISPLAY_PROPERTIES']['vin']['VALUE']?>" <?endif;?>
 >
-    <?$brandp = explode(" ", $arResult['NAME']);
-    $brand = $brandp[0];
-    if ($brand == 'Land') {
-        $brand = 'Land Cruser';
-    }
-    ?>
-    <span itemprop="brand" style="display:none;"><?=$brand?></span>
+    <span itemprop="brand" style="display:none;"><?=$arResult['NAME']?></span>
+
     <div class="container clearfix">
+
         <div class="card_car_left">
             <div class="thumbs">
-                <?$ind=0;foreach($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'] as $idx=>$img): $ind = $idx;?>
-                <a href="<?=$img['SRC']?>" class="thumbs_item" style="background-image: url(<?=$img['SRC_PREW']?>);"></a><?//Олег: заменил $img['SRC'] на $img['SRC_PREW'] в котором уменьшеная картинка?>
+                <? foreach($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE'] as $idx => $img): ?>
+                    <a href="<?=$img['SRC']?>" class="thumbs_item" style="background-image: url(<?=$img['SRC_PREW']?>);"></a>
                 <?endforeach?>
+
                 <img style="display:none;" itemprop="image" src="<?=$img['SRC']?>" loading="lazy" width="586" height="390" />
-                <?if($ind > 8):?><a class="more_photos" href="javascript:void(0);">Ещё <?=($ind-9);?> фото</a><?endif;?>
+
+                <? if(count($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE']) > 10): ?>
+                    <a class="more_photos" href="javascript:void(0);">
+                        Ещё <?=(count($arResult['DISPLAY_PROPERTIES']['img']['FILE_VALUE']) - 9);?> фото
+                    </a>
+                <?endif;?>
             </div>
         </div>
+
         <div class="card_car_right">
 
             <ul class="breadcrumbs" itemscope="" itemtype="http://schema.org/BreadcrumbList" id="breadcrumbs">
@@ -59,13 +59,9 @@ $APPLICATION->AddHeadString('<link href="https://'.$_SERVER["SERVER_NAME"].$cnao
                     </a>
                 </li>
             </ul>
+
             <h1 class="card_car_title" itemprop="name"><?=$arResult['NAME'];?></h1>
-            <?if($arResult['DATE_CREATE']):?>
-                <div class="card_car_date">
-                    <!--Добавлен <? echo FormatDateFromDB($arResult["DATE_CREATE"], 'SHORT'); ?>-->
-                    &nbsp;
-                </div>
-            <?endif;?>
+
             <div class="char_box clearfix">
                 <div class="char_left" itemprop="description">
                     <?if($arResult['DISPLAY_PROPERTIES']['year']['VALUE']):?>
@@ -135,7 +131,6 @@ $APPLICATION->AddHeadString('<link href="https://'.$_SERVER["SERVER_NAME"].$cnao
                 </div>
             </div>
 
-
             <div itemprop="offers" itemscope itemtype="http://schema.org/Offer" style="display:none;">
                 <div><?=$arResult['DISPLAY_PROPERTIES']['price']['VALUE']?> ₽</div>
                 <meta itemprop="price" content="<?=$arResult['DISPLAY_PROPERTIES']['price']['VALUE']?>.00">
@@ -144,60 +139,44 @@ $APPLICATION->AddHeadString('<link href="https://'.$_SERVER["SERVER_NAME"].$cnao
             </div>
 
             <div class="card_car_price_car">
-
-
                 <div class="card_car_price_car_left">
                     <?if(!empty($arResult['DISPLAY_PROPERTIES']['oldprice']['VALUE'])):?>
                         <div class="card_car_old_price">
-                            <?=number_format($arResult['DISPLAY_PROPERTIES']['oldprice']['VALUE'], 0, ',', ' ');?>
+                            <?=CurrencyFormat($arResult['DISPLAY_PROPERTIES']['oldprice']['VALUE'], 'RUB');?>
                         </div>
                     <?endif;?>
                     <div class="card_car_new_price">
-                        <?=number_format($arResult['DISPLAY_PROPERTIES']['price']['VALUE'], 0, ',', ' ');?> ₽
+                        <?=CurrencyFormat($arResult['DISPLAY_PROPERTIES']['price']['VALUE'], 'RUB');?>
                     </div>
-
-                    <? $time = time(); ?>
-                    <?if($time < 1577836799):?>
-                        <?if($arResult['DISPLAY_PROPERTIES']['city']['VALUE'] == 'Воронеж'):?>
-                            <div class="car_item_persent">Рассрочка 0%*</div>
-                        <?endif;?>
-                    <?endif;?>
                 </div>
+
                 <div class="card_car_price_car_right">
                     <a href="javascript:void(0);"
                        onclick="show_popupTest('<?=str_replace(["'"], '', $arResult['NAME']);?> <?=$arResult['DISPLAY_PROPERTIES']['year']['VALUE']?>', <?=$arResult['DISPLAY_PROPERTIES']['filial']['VALUE'];?>)"
-                       class="red_btn">
-                        <?//=(CITY_NAME == 'Воронеж' || CITY_NAME == 'Санкт-Петербург')? "Купить онлайн":"Рассчитать trade-in"?> <?// Олег: БЫЛО заменил название кнопки в зависимости от города?>
-                        Рассчитать trade-in<?// Олег: СТАЛО одно название кнопки на все города?>
-                    </a>
+                       class="red_btn">Рассчитать trade-in</a>
+
                     <a href="javascript:void(0);"
                        onclick="show_popup('<?=str_replace(["'"], '', $arResult['NAME']);?> <?=$arResult['DISPLAY_PROPERTIES']['year']['VALUE']?>', <?=$arResult['DISPLAY_PROPERTIES']['filial']['VALUE'];?>);"
                        class="white_btn">Рассчитать кредит</a>
                 </div>
             </div>
-            <!-- <p class="car_card-red-text">скидка распространяется при покупке автомобиля в кредит</p> -->
+
+            <? if($arResult['FILIAL']): ?>
             <div class="card_car_map">
                 <div class="card_car_map_title">Где можно осмотреть авто</div>
-                <span><?=$arResult['FILIAL'][0]['NAME'];?>,<br/>
-                        тел <a href="tel:<?=$arResult['FILIAL'][0]['PROPERTY_PHONE_VALUE'];?>" style="color: #ea1e26">
-                        <?=$arResult['FILIAL'][0]['PROPERTY_PHONE_VALUE'];?></a><br/>
-                        <?=$arResult['FILIAL'][0]['PROPERTY_ADDRESS_VALUE'];?></span>
-                <?
-                if(isset($arResult['FILIAL'][0]['PROPERTY_COORD_VALUE'])):
-                    $coord = $arResult['FILIAL'][0]['PROPERTY_COORD_VALUE'];
-                    $coord = explode(',', $coord);
-                    $city = '';
-                    if(isset($arResult['FILIAL'][0]['PROPERTY_CITY_VALUE'])){
-                        $city = $arResult['FILIAL'][0]['PROPERTY_CITY_VALUE'] . ', ';
-                    }
-                    ?>
+                <span>
+                    <?=$arResult['FILIAL']['NAME'];?>,<br/>
+                    тел <a href="tel:<?=$arResult['FILIAL']['PROPERTY_PHONE_VALUE'];?>" style="color: #ea1e26">
+                    <?=$arResult['FILIAL']['PROPERTY_PHONE_VALUE'];?></a><br/>
+                    <?=$arResult['FILIAL']['PROPERTY_ADDRESS_VALUE'];?>
+                </span>
 
-                    <? $sub = array_shift((explode(".",$_SERVER['HTTP_HOST']))); ?>
-
-                    <div onclick="return false;" style="cursor:unset" class="map<? if( $sub == 'spb' ) { ?> map-spb<? } ?><? if( $sub == 'belgorod' ){ ?> map-bel<? } ?>"></div>
-
+                <? if(isset($arResult['FILIAL']['PROPERTY_COORD_VALUE'])): ?>
+                    <div onclick="return false;" style="cursor:unset" class="map"></div>
                 <?endif;?>
             </div>
+            <? endif; ?>
+
         </div>
 
         <div class="advantages">
@@ -209,7 +188,6 @@ $APPLICATION->AddHeadString('<link href="https://'.$_SERVER["SERVER_NAME"].$cnao
 
         <?if(!empty($arResult['equipment'])):?>
             <h2>Комплектация автомобиля</h2>
-
             <div class="compl_auto">
                 <?
                 $inc = count($arResult['equipment']);
@@ -234,3 +212,5 @@ $APPLICATION->AddHeadString('<link href="https://'.$_SERVER["SERVER_NAME"].$cnao
         <? endif; ?>
 
     </div>
+
+</div>
